e7a48fd3b100ff2e7cf345fc58e6024c36187cb5
bilo <bbesir@hotmail.com>
2022-10-07 18:41:55 +0300

    EJS

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const {parse} = require("path");
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.set('view engine','ejs');
+
+app.get("/", function (req, res) {
+  res.render("index.ejs",{buttonBaslik:"BBB"});
+});
+
+app.post("/", function (req, res) {});
+
+app.listen(3000, () => console.log("Server is online and listening at port 3000."));
+
+// app.listen(3000, function () {
+//   console.log("Server is online and listening at port 3000.");
+// });
